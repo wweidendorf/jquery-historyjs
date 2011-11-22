7c97b9cc33ee8667099a47934955eafdd6e682bb
William Weidendorf <will@xspond.com>
2011-11-22 17:49:25 -0500

    Bump

--- lib/historyjs/rails/version.rb
+++ lib/historyjs/rails/version.rb
@@ -1,8 +1,7 @@
 module Historyjs
   module Rails
-    VERSION               = "0.1.0"
-    HISTORYJS_VERSION     = "1.7.0"
+    VERSION               = "0.2.0"
+    HISTORYJS_VERSION     = "1.7.1-r2"
     JSON2_VERSION         = "8e0b15cb492f63067a88"
-    AMPLIFY_STORE_VERSION = "1.0beta"
   end
 end

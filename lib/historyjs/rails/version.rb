7b20575f565cc68432e9a4b438d0897b3da54118
William Weidendorf <will@xspond.com>
2012-03-09 06:56:59 -0500

    Bump version of JSON2

--- lib/historyjs/rails/version.rb
+++ lib/historyjs/rails/version.rb
@@ -2,6 +2,6 @@ module Historyjs
   module Rails
     VERSION               = "0.2.2"
     HISTORYJS_VERSION     = "1.7.1-r2"
-    JSON2_VERSION         = "8e0b15cb492f63067a88"
+    JSON2_VERSION         = "47a9882cdd"
   end
 end

7f2875360e12e7c83e3ca0784b57fd39cadfc8df
William Weidendorf <will@xspond.com>
2012-03-09 06:57:21 -0500

    Remove amplify store references in install generator

--- lib/generators/historyjs/install/install_generator.rb
+++ lib/generators/historyjs/install/install_generator.rb
@@ -3,7 +3,7 @@ require 'rails'
 module Historyjs
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
-      desc "This generator installs HistoryJS #{Historyjs::Rails::HISTORYJS_VERSION}, JSON2, and Amplify Store #{Historyjs::Rails::AMPLIFY_STORE_VERSION}"
+      desc "This generator installs HistoryJS #{Historyjs::Rails::HISTORYJS_VERSION} and JSON2"
       source_root File.expand_path('../../../../../vendor/assets/javascripts', __FILE__)
 
       def copy_historyjs
@@ -18,12 +18,6 @@ module Historyjs
         say_status("copying", "JSON2 (#{Historyjs::Rails::JSON2_VERSION})", :green)
         copy_file "json2.js", "public/javascripts/json2.js"
       end
-
-      def copy_amplify_store
-        say_status("copying", "Amplify Store (#{Historyjs::Rails::AMPLIFY_STORE_VERSION[0..5]})", :green)
-        copy_file "amplify_store.js", "public/javascripts/amplify.store.js"
-      end
-
     end
   end
 end if ::Rails.version < "3.1"
